0a31fb21b376045e623aa95fbc9d506357d3b38e
SebastianCG19 <116407682+SebastianCG19@users.noreply.github.com>
2024-09-30 18:42:32 -0500

    PracticeExam

---
+++ PractiExam/app/src/main/java/com/example/practiexam/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.practiexam
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practiexam.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnAction.setOnClickListener {
+
+            val textToShow = "${binding.etNombre.text}: ${binding.etEp1.text}"
+            Utils.showToast(this,textToShow)
+            //Toast.makeText(this,textoToShow, Toast.LENGTH_LONG).show()
+
+
+            var ep1 = binding.etEp1.text.toString().toInt()
+            if (binding.rbAdd.isChecked){
+                ep1 = ep1++
+            }
+
+
+            val newView= Intent(this, Destino::class.java)
+            newView.putExtra("nombre", "${binding.etNombre.text}")
+            newView.putExtra("ep1", "$ep1")
+            newView.putExtra("ep2", "${binding.etEp2.text}")
+            startActivity(newView)
+
+        }
+
+
+    }
+}
